825178be9f750ccfe7aeb48d0199f0f8e12993a8
Diogo Bedin Carlesso <dig.skull@gmail.com>
2022-04-13 00:46:44 -0300

    Created CommandRepo and Models

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,23 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data;
+
+public interface ICommandRepo
+{
+    bool SaveChanges();
+
+    // Platforms
+    IEnumerable<Platform> GetAllPlatforms();
+
+    void CreatePlatforms(Platform platform);
+
+    bool PlatformExists(int platformId);
+
+    // Commands
+    IEnumerable<Command> GetCommandsPlatform(int platformId);
+    
+    Command GetCommand(int platformId, int commandId);
+
+    void CreateCommand(int platformId, Command command);
+
+}
